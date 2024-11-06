7568575b8d9b5a191d7ac56c61974f71a77a6032
cone26 <sight41siyah@gmail.com>
2024-11-06 17:05:16 +0000

    edit: edit list using useState

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import "./App.css";
 
 function App() {
   const [todo, setTodo] = React.useState("");
-  const onChange = (event) => {
-    setTodo(event.target.value);
-  };
+  const [todos, setTodos] = React.useState([]);
+
+  const onChange = (event) => setTodo(event.target.value);
   const addTodo = (e) => {
     e.preventDefault();
-    const ul = document.getElementById("todoList");
-    const li = document.createElement("li");
-    const btn = document.getElementsByTagName("input")[0];
-    li.innerText = todo;
-    ul.appendChild(li);
-    btn.value = "";
+    if (todo === "") return;
+
+    setTodo("");
+    setTodos((todos) => [todo, ...todos]);
   };
 
   return (
     <div className="App">
-      <form>
+      <h1>Todo List</h1>
+      <form onSubmit={addTodo}>
         <input
+          onChange={onChange}
+          value={todo}
           type="text"
           placeholder="Type what you should do..."
-          onChange={onChange}
         ></input>
-        <button type="submit" onClick={addTodo}>
-          Add todo
-        </button>
+        <button>Add todo</button>
       </form>
-      <ul id="todoList"></ul>
+      {todos.map((todo, id) => (
+        <li key={id}>{todo}</li>
+      ))}
     </div>
   );
 }
