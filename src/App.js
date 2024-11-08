01b3ab28c26cdfa8e86a81989072d5b766011bc4
cone26 <sight41siyah@gmail.com>
2024-11-08 21:57:52 +0000

    add: deelte todo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [todo, setTodo] = React.useState("");
   const [todos, setTodos] = React.useState([]);
   // const [percent, setPercent] = React.useState(0);
+  const [status, setStatus] = React.useState(true);
+
   const isDone = " - ";
   const onChange = (event) => setTodo(event.target.value);
   const addTodo = (e) => {
@@ -13,6 +15,10 @@ function App() {
 
     setTodo("");
     setTodos((todos) => [todo, ...todos]);
+  };
+  const deleteTodo = (index) => {
+    const updatedTodo = todos.filter((_, i) => i !== index);
+    setTodos([...updatedTodo]);
   };
 
   return (
@@ -33,10 +39,10 @@ function App() {
         ></input>
         <button>Add todo</button>
       </form>
-
+      <span>Click to Check Done</span>
       <ul className="todos">
-        {todos.map((todo, id) => (
-          <li key={id}>
+        {todos.map((todo, idx) => (
+          <li key={idx} onClick={() => deleteTodo(idx)}>
             {isDone}
             {todo}
           </li>
