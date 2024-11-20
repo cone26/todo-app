879a5c26162dae2bb0f493bce2b9d71ecf8bb6ea
cone26 <sight41siyah@gmail.com>
2024-11-20 22:04:52 +0000

    edit: ci cd

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,11 @@ import type { NextConfig } from "next";
 
 module.exports = {
   distDir: "dist",
-  output: "export",
+  // output: "export",
 };
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  trailingSlash: true,
-  images: {
-    loader: "default",
-    // domains: ["your-domain.com"],
-  },
 };
 
 export default nextConfig;
